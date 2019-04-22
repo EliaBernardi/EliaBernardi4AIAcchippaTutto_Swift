34c8f38931b9af1c229a96465159f0a660737e98
Elia <elia.bernardi01@gmail.com>
2019-04-22 20:41:57 +0200

    Inizio codice

--- AcchiappaTutto/AcchiappaTutto/ViewController.swift
+++ AcchiappaTutto/AcchiappaTutto/ViewController.swift
@@ -13,7 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        var randomAltezza: Int = Int.random(in: 0 ..< Int(view.frame.height))
+        var randomLarghezza: Int = Int.random(in: 0 ..< Int(view.frame.width))
+
+        var button = UIButton(frame: CGRect(x: randomLarghezza, y: randomAltezza, width: 130, height: 130))
+        
+        
+        button.setTitle("Prova", for: .normal)
+        button.titleLabel!.font =  UIFont(name: ".SFUIText", size: 50)
+        self.view.addSubview(button)
+    
     }
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
