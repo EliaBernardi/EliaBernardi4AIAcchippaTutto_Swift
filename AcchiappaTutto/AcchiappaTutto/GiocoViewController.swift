09fba4bffa1c2f853df756b46a5fba3b9c005354
Elia <elia.bernardi01@gmail.com>
2019-04-26 15:50:07 +0200

    Array con le posizioni dei button

--- AcchiappaTutto/AcchiappaTutto/GiocoViewController.swift
+++ AcchiappaTutto/AcchiappaTutto/GiocoViewController.swift
@@ -16,18 +16,45 @@ class GiocoViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
+        //var randomAltezza: Int = Int.random(in: 0 ..< Int(view.frame.height))
+        //var randomLarghezza: Int = Int.random(in: 0 ..< Int(view.frame.width))
         
-        var randomAltezza: Int = Int.random(in: 0 ..< Int(view.frame.height))
-        var randomLarghezza: Int = Int.random(in: 0 ..< Int(view.frame.width))
+        //W H : 40/580   169/585   303/582
+        //W h : 41/701   179/705   312/701
         
-        let button = UIButton(frame: CGRect(x: randomLarghezza, y: randomAltezza, width: 130, height: 130))
+
+        let Posizione1 = CGRect(x: 40, y: 580, width: 84, height: 90)
+        let Posizione2 = CGRect(x: 169, y: 585, width: 84, height: 90)
+        let Posizione3 = CGRect(x: 303, y: 582, width: 84, height: 90)
+        let Posizione4 = CGRect(x: 41, y: 701, width: 84, height: 90)
+        let Posizione5 = CGRect(x: 179, y: 705, width: 84, height: 90)
+        let Posizione6 = CGRect(x: 312, y: 701, width: 84, height: 90)
+        var CoordinateButton = [CGRect]()
+        CoordinateButton.append(Posizione1)
+        CoordinateButton.append(Posizione2)
+        CoordinateButton.append(Posizione3)
+        CoordinateButton.append(Posizione4)
+        CoordinateButton.append(Posizione5)
+        CoordinateButton.append(Posizione6)
         
+        
+        for i in 0...5
+        {
+            let button = UIButton(frame: CoordinateButton[i])
+            let image = UIImage(named: "Talpa3")
+            button.setImage(image, for: .normal)
+            self.view.addSubview(button)
+            self.view.bringSubview(toFront: button)
+        }
+        
+        
+        //let button = UIButton(frame: CGRect(x: randomLarghezza, y: randomAltezza, width: 84, height: 90))
+        /*
         let image = UIImage(named: "Talpa3")
         button.setImage(image, for: .normal)
-        //button.titleLabel!.font =  UIFont(name: ".SFUIText", size: 50)
         self.view.addSubview(button)
         self.view.bringSubview(toFront: button)
-        
+       */
     }
     
 
